bfb7ea90d86915cb6a3b38bf37e748b2a7dfafdc
Valdir <valdir.nat@live.com>
2019-02-14 15:06:27 -0200

    autoload do hcodebr classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf79e251ec6315758eccae4029dfd3b06
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitf79e251ec6315758eccae4029dfd3b06
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitf79e251ec6315758eccae4029dfd3b06::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf79e251ec6315758eccae4029dfd3b06::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf79e251ec6315758eccae4029dfd3b06::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitf79e251ec6315758eccae4029dfd3b06::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitf79e251ec6315758eccae4029dfd3b06::$classMap;
 
